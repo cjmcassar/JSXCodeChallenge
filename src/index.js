0e17795ab62e4ac97feffbc57993a3f516f987d8
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2020-12-30 19:05:01 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Banana</li>
+      <li>Peanut Butter</li>
+      <li>Chai</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
